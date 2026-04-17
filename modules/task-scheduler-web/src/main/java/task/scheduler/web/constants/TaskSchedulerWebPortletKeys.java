0e83cfad6f0db4da5c51f6aebb39efdcce63d6b0
Izarbe Bailo Aysa <ibailo@hiberus.com>
2026-04-17 15:09:04 +0200

    scheduler initialized

---
+++ modules/task-scheduler-web/src/main/java/task/scheduler/web/constants/TaskSchedulerWebPortletKeys.java
@@ -0,0 +1,11 @@
+package task.scheduler.web.constants;
+
+/**
+ * @author IzarbeBailoAysa
+ */
+public class TaskSchedulerWebPortletKeys {
+
+	public static final String TASKSCHEDULERWEB =
+		"task_scheduler_web_TaskSchedulerWebPortlet";
+
+}
